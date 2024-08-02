31254fbce026c4e35879a927405bf16689d316de
someshwar <someshwarsholkar2004@gmail.com>
2024-08-02 09:10:47 +0530

    Frontend Add

--- model/Projects.js
+++ model/Projects.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const blogSchema = new mongoose.Schema({
     name: { type: String, required: true },
     hero: { type: String, required: true },
-    desc: { type: String, required: true }
+    desc: { type: String, required: true },
+    url: { type: String, required: true }
 })
 module.exports = mongoose.model("Blog", blogSchema)
